46ee8a3084da7792771e2e797d1b0db8734981ee
Gunratna More <gunratnamore@gmail.com>
2022-08-21 00:34:18 +0530

    last push

---
+++ src/pages/Home.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success(' 😎 Created a new room have fun');
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error('😢ROOM ID & username is required');
+      return;
+    }
+
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === 'Enter') {
+      joinRoom();
+    }
+  };
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code_colab2.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">ENTER ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+          <span className="createInfo">
+            Don't have ID ? No Worries Create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Made by{' '}
+          <a href="https://linkedin.com/in/gunratna-more-9a4837196">
+            {' '}
+            Gunratna More
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
